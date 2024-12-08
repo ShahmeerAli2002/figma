109a1c57a7ffc41d181fbd58c080a05cd7c6ed3c
Shahmeer Ali <problemssolve196@gmail.com>
2024-12-08 23:29:28 +0500

    ,/

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -9,21 +9,25 @@ const MobileNavbar = () => {
     <>
       <span
         onClick={() => setDisplay((val) => !val)}
-        className="h-8 w-8 sm:hidden flex items-center justify-center"
+
+        className="h-10 w-10 sm:hidden flex items-center justify-center rounded-full bg-indigo-100 hover:bg-indigo-200"
       >
-        <IoMenuSharp className="h-4/5 w-4/5" />
+
+        <IoMenuSharp className="h-6 w-6 text-indigo-800" />
       </span>
       <span
         className={`${
           display ? "translate-x-0" : "-translate-x-full"
-        } duration-500 fixed z-50 top-0 left-0 bg-black/90 h-screen w-4/5 sm:hidden inline-block`}
+
+        } duration-300 fixed z-50 top-0 left-0 bg-gradient-to-b from-purple-900/95 to-indigo-800/95 h-screen w-[90%] sm:hidden inline-block backdrop-blur-sm`}
       >
-        <ul className="h-full flex flex-col justify-evenly items-center">
+        <ul className="h-full flex flex-col justify-center gap-8 items-center">
           {["home", "about", "products", "cart"].map((val, ind) => (
             <Link key={ind} href={`/${val === "home" ? "" : val}`}>
               <li
                 onClick={() => setDisplay(false)}
-                className="capitalize font-clash text-lightGray"
+
+                className="capitalize font-clash text-white text-xl hover:text-indigo-300 transition-colors duration-200 tracking-wider"
               >
                 {val}
               </li>
